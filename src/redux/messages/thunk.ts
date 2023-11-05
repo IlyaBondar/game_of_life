c6fac200707dba850649c814df2d9205d50ed3bd
Ilya Bondar <ilya.bondar@ihsmarkit.com>
2023-11-05 23:44:40 +0100

    error handling

--- src/redux/messages/thunk.ts
+++ src/redux/messages/thunk.ts
@@ -8,19 +8,24 @@ export const getGPTAnswer = (id: string) => async (dispatch: AppDispatch, getSta
     const allMessages = getMessages(state);
     const messages = convertToGTP(getMessageBefore(id, allMessages));
     let response = '';
-
-    const eventSource = new EventSource(`/api/gpt?messages=${JSON.stringify(messages)}`);
-    eventSource.addEventListener("message", (e) => {
-        const value = decodeURIComponent(e.data)
-        response = `${response}${value}`;
-        dispatch(updateMessage({ id, content: response }));
-    });
-    eventSource.addEventListener("end", () => {
-        dispatch(updateMessage({ id, parsing: true }));
-        eventSource.close();
-    });
-    eventSource.addEventListener("error", (e:MessageEvent) => {
-        eventSource.close();
-        console.error(e.data || e)
-    });
+    if (window.EventSource) {
+        const eventSource = new EventSource(`/api/gpt?messages=${JSON.stringify(messages)}`);
+        eventSource.addEventListener("message", (e) => {
+            const value = decodeURIComponent(e.data)
+            response = `${response}${value}`;
+            dispatch(updateMessage({ id, content: response }));
+        });
+        eventSource.addEventListener("end", () => {
+            dispatch(updateMessage({ id, parsing: true }));
+            eventSource.close();
+        });
+        eventSource.addEventListener("error", (e:MessageEvent) => {
+            eventSource.close();
+            console.error(e.data || e);
+        });
+    } else {
+        const errorMessage = `Error: EventSource isn't supported by your browser`
+        console.error(errorMessage);
+        dispatch(updateMessage({ id, content: errorMessage }));
+    }
 }
