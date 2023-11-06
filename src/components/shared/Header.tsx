6b4e9277a70c6e0c7d69476af2dd6cf61c56f4a9
Ilya Bondar <ilya.bondar@ihsmarkit.com>
2023-11-06 12:06:03 +0100

    navigation

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { NavBarButtons } from "../login/NavBarButtons";
-import Link from "../shared/Link";
+import NavLink from "../shared/Link";
 
 export default function Header() {
     return (<header className="flex w-full max-w-screen-md mx-auto justify-between sticky top-0 bg-black">
         <nav className="flex gap-4 my-1">
-            <Link href="/" >Chat</Link>
-            <Link href="/game">Game</Link>
+            <NavLink href="/" >Chat</NavLink>
+            <NavLink href="/game">Game</NavLink>
         </nav>
         <NavBarButtons/>
     </header>)
--- src/components/shared/Link.tsx
+++ src/components/shared/Link.tsx
@@ -1,18 +1,22 @@
 import clx from 'classnames';
 import { default as NextLink } from 'next/link';
+import { usePathname } from 'next/navigation';
 
 type NextLinkProps = React.ComponentProps<typeof NextLink>;
 
-export default function Link(props:NextLinkProps) {
-    const { className } = props;
+export default function NavLink(props:NextLinkProps) {
+    const pathname = usePathname();
+    const { className, href } = props;
+    const isActive = pathname === href;
     return (<NextLink
         {...props}
         className={clx(
             `pointer-events-auto rounded-md
             border border-indigo-200
             px-3 py-2 text-[0.8125rem] font-semibold leading-5
-            text-white hover:text-indigo-500
+            hover:text-indigo-500
             disabled:border-gray-100 disabled:text-gray-400`,
+            isActive ? 'text-indigo-300' : 'text-white',
             className)}/>
     )
 }
